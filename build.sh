1972235b05500c48cab4d72f2d35359f2ccd084d
Luis Mayta <luis.mayta@drinux.com>
2018-12-19 19:14:51 -0500

    corre

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 docker run -dit --name elk debin:latest
 
-docker build -t lfmc/elk:6.5.3 .
+docker build -f Dockerfile0 -t lfmc/elk:1 .
+docker build -f Dockerfile -t lfmc/elk:2 .
 
-docker run --name srv-elk -dit -p 9200:9200 lfmc/elk:6.5.3
+docker run --name elk-1 -dit -p 90:9200 lfmc/elk:1
 docker run -p 5601:5601 -p 9201:9200 -p 5044:5044 -it --name elk sebp/elk
+
+docker exec -it elk-1 bash
